a182500be7453d001a6cd49d2a01a1253b959074
salbyul <kpast96@gmail.com>
2023-07-09 20:27:52 +0900

    refactor(racing-car): 출력메시지 추가

--- src/main/java/racing/InputView.java
+++ src/main/java/racing/InputView.java
@@ -8,6 +8,8 @@ public class InputView {
     public static final String DELIMITER = ",";
     public static final String NOT_NUMBER_MESSAGE = "숫자만 입력할 수 있습니다.";
     public static final String NOT_POSITIVE_NUMBER_MESSAGE = "1 이상의 숫자만 입력할 수 있습니다.";
+    public static final String GET_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    public static final String GET_NUMBER_MESSAGE = "시도할 횟수는 몇 회인가요?";
     private final Scanner scanner;
 
     public InputView(Scanner scanner) {
@@ -15,11 +17,13 @@ public class InputView {
     }
 
     public String[] getCarNames() {
+        System.out.println(GET_CAR_NAME_MESSAGE);
         String input = scanner.nextLine();
         return input.split(DELIMITER);
     }
 
     public int getNumber() {
+        System.out.println(GET_NUMBER_MESSAGE);
         String number = scanner.nextLine();
         try {
             int parsedInteger = Integer.parseInt(number);
