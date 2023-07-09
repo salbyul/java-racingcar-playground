b0269986e1e425e3252546072a8bb020d094b94c
salbyul <kpast96@gmail.com>
2023-07-09 20:24:02 +0900

    feat(racing-car): 시도할 횟수 입력받기

--- src/main/java/racing/InputView.java
+++ src/main/java/racing/InputView.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class InputView {
 
+    private static final int MIN_COUNT = 1;
+    public static final String DELIMITER = ",";
+    public static final String NOT_NUMBER_MESSAGE = "숫자만 입력할 수 있습니다.";
+    public static final String NOT_POSITIVE_NUMBER_MESSAGE = "1 이상의 숫자만 입력할 수 있습니다.";
     private final Scanner scanner;
 
     public InputView(Scanner scanner) {
@@ -12,6 +16,23 @@ public class InputView {
 
     public String[] getCarNames() {
         String input = scanner.nextLine();
-        return input.split(",");
+        return input.split(DELIMITER);
+    }
+
+    public int getNumber() {
+        String number = scanner.nextLine();
+        try {
+            int parsedInteger = Integer.parseInt(number);
+            validateNumber(parsedInteger);
+            return parsedInteger;
+        } catch (NumberFormatException e) {
+            throw new RuntimeException(NOT_NUMBER_MESSAGE);
+        }
+    }
+
+    private void validateNumber(int parsedInteger) {
+        if (parsedInteger < MIN_COUNT) {
+            throw new IllegalArgumentException(NOT_POSITIVE_NUMBER_MESSAGE);
+        }
     }
 }
--- src/test/java/racing/InputViewTest.java
+++ src/test/java/racing/InputViewTest.java
@@ -1,15 +1,17 @@
 package racing;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class InputViewTest {
 
@@ -20,10 +22,49 @@ public class InputViewTest {
         System.setIn(generateInputStream(input));
         InputView inputView = new InputView(new Scanner(System.in));
         String[] names = inputView.getCarNames();
-        Assertions.assertThat(names).containsExactly("Bruno Mars", "Justin Bieber", "DJ khaled");
+        assertThat(names).containsExactly("Bruno Mars", "Justin Bieber", "DJ khaled");
+    }
+
+    @ParameterizedTest
+    @DisplayName("시도할 횟수 입력받기")
+    @ValueSource(ints = {1, 10, 30})
+    void getNumber(int number) {
+        System.setIn(generateInputStream(number));
+        InputView inputView = new InputView(new Scanner(System.in));
+        int input = inputView.getNumber();
+        assertThat(input).isEqualTo(number);
+    }
+
+    @ParameterizedTest
+    @DisplayName("숫자가 아닌 횟수 입력받기")
+    @CsvSource(value = {"a", "-"})
+    void validateNotNumber(String input) {
+        System.setIn(generateInputStream(input));
+        InputView inputView = new InputView(new Scanner(System.in));
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            inputView.getNumber();
+        });
+        assertThat(exception.getMessage()).isEqualTo("숫자만 입력할 수 있습니다.");
+    }
+
+    @ParameterizedTest
+    @DisplayName("0 이하의 횟수 입력받기")
+    @ValueSource(ints = {0, -1, -100})
+    void validateNotPositiveNumber(int input) {
+        System.setIn(generateInputStream(input));
+        InputView inputView = new InputView(new Scanner(System.in));
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            inputView.getNumber();
+        });
+        assertThat(exception.getMessage()).isEqualTo("1 이상의 숫자만 입력할 수 있습니다.");
     }
 
     static InputStream generateInputStream(String input) {
         return new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
+    }
+
+    static InputStream generateInputStream(int input) {
+        String parsedString = String.valueOf(input);
+        return new ByteArrayInputStream(parsedString.getBytes(StandardCharsets.UTF_8));
     }
 }
